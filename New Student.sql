401bf221b650187295ba9ec93aafa476c76142e0
saiefzaier <45342940+saiefzaier@users.noreply.github.com>
2019-07-12 02:56:44 -0700

    Delete New Student.sql

--- New Student.sql
+++
@@ -1,9 +0,0 @@
-
-accept ns_id prompt 'enter studentid';
-accept ns_fname prompt 'enter student firstname';
-accept ns_lname prompt 'enter student lastname';
-accept ns_age prompt 'enter student age';
-accept ns_address prompt 'enter student address';
-begin
-UNIVERSITY.new_student (&ns_id,'&ns_fname','&ns_lname',&ns_age,'&ns_address');
-end;
